0bceaf0cfa3dbbd1e243ff36d84081d83584fd50
DaniyalAli1 <dannyalives2003@gmail.com>
2024-05-10 12:26:33 +0500

    third commit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,6 +3,8 @@
 import { Header } from "@/components/Header";
 import { useUser } from "@clerk/nextjs";
 import React from "react";
+import { currentUser } from '@clerk/nextjs/server';
+
 
 const DashboardLayout = ({ children}: {children: React.ReactNode}) => {
   const {user} = useUser();
@@ -15,4 +17,12 @@ const DashboardLayout = ({ children}: {children: React.ReactNode}) => {
   )
 }
 
+async function Page() {
+  const user = await currentUser();
+
+  if (!user) return <div>Not signed in</div>;
+
+  return <div>
+    console({user?.username}) </div>;
+}
 export default DashboardLayout
